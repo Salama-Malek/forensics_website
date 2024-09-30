d4ddde6801416d3c99297e72ad667651ee72de72
unknown <salamahassanein@gmail.com>
2024-09-30 19:46:31 +0300

    Clearing all files

--- analysis/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Evidence, MalwareAnalysis, LogFile
-
-admin.site.register(Evidence)
-admin.site.register(MalwareAnalysis)
-admin.site.register(LogFile)
--- analysis/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='endswith')
-def endswith(value, arg):
-    """Returns True if the value ends with the argument string."""
-    return str(value).lower().endswith(arg)
